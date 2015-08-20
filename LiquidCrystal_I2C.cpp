58667b65e518c4fee7cfc97d18e7e1cc96c11f95
marcoschwartz <marcolivier.schwartz@gmail.com>
2015-08-20 07:59:32 +0200

    initial commit

--- LiquidCrystal_I2C.cpp
+++ LiquidCrystal_I2C.cpp
@@ -1,11 +1,4 @@
-//YWROBOT
-//last updated on 21/12/2011
-//Tim Starling Fix the reset bug (Thanks Tim)
-//wiki doc http://www.dfrobot.com/wiki/index.php?title=I2C/TWI_LCD1602_Module_(SKU:_DFR0063)
-//Support Forum: http://www.dfrobot.com/forum/
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
-
+// Based on the work by DFRobot
 
 #include "LiquidCrystal_I2C.h"
 #include <inttypes.h>
